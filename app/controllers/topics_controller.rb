1f0a6e74716a4b1d1c3aebb77820140070042eb3
Jordan Singer <singer.jordan@gmail.com>
2015-04-18 22:55:42 -0700

    Completed controller methods to handle JSON responses.

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,32 +1,41 @@
 class TopicsController < ApplicationController
-	before_filter :set_topic, only: [:destroy]
+  before_filter :set_topic, only: [:destroy]
+  skip_before_filter :verify_authenticity_token
 
-	def index
-		unless params[:query]
-			render :json => Topic.all # show all topics
-		else
-			render :json => Topic.search(params[:query]) # search topics
-		end
-	end
+  def index
+    unless params[:query]
+      render :json => Topic.all # show all topics
+    else
+      render :json => Topic.search(params[:query]) # search topics
+    end
+  end
 
-	def create
-		@topic = Topic.new(topic_params)
-	end
+  def create
+    @topic = Topic.new(topic_params)
 
-	def load
-		Topic.load_topics
-	end
+    if @topic.save
+      render :json => @topic
+    else
+      render :json => @topic.errors
+    end
+  end
 
-	def destroy
-		@topic.destroy
-	end
+  def load
+    @topics = Topic.load_topics
+    render :json => @topics
+  end
 
-	private
-	def set_topic
-		@topic = Topic.find(params[:id])
-	end
+  def destroy
+    @topic.destroy
+    head :no_content
+  end
 
-	def topic_params
-		params.require(:topic).permit(:name, :query)
-	end
+  private
+  def set_topic
+    @topic = Topic.find(params[:id])
+  end
+
+  def topic_params
+    params.require(:topic).permit(:name, :query)
+  end
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,11 +2,12 @@ class Topic < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
 
   # Twitter client
+  TWITTER_CONFIG = YAML.load_file("#{Rails.root}/config/twitter.yml")
   TWITTER = Twitter::REST::Client.new do |config|
-    config.consumer_key = "uOkHhUZFnTKUL8yHMYVQ"
-    config.consumer_secret = "pviGp2WzX7l0rhMX2G9DkeBrCya8NeoR7br1kjPgV8k"
-    config.access_token = "35623579-xUvaXUafxBFEhkkowJoOTBkcT17JJr4hdaXsE5IVI"
-    config.access_token_secret = "khnTAZzRbLUzjTdPwAmjdhtmBXCTBZkYVOokp61hKo"
+    config.consumer_key = TWITTER_CONFIG['config']['consumer_key']
+    config.consumer_secret = TWITTER_CONFIG['config']['consumer_secret']
+    config.access_token = TWITTER_CONFIG['config']['access_token']
+    config.access_token_secret = TWITTER_CONFIG['config']['access_token_secret']
   end
 
   # Fetches trends from Twitter, stores them
@@ -20,5 +21,18 @@ class Topic < ActiveRecord::Base
   # Searches data store for provided query
   def self.search(query)
     self.where(["name LIKE ?", "#{query}%"])
+  end
+
+  # Twitter search URL for a given query
+  def url
+    "http://twitter.com/search/?q=" + self.query
+  end
+
+  # Return standard topic JSON
+  def serializable_hash(options={})
+    options = {
+      :only => [:id, :name, :query], :methods => [:url]
+    }.update(options)
+    super(options)
   end
 end
